3645413b42e17f06a9277b9dd8cf85439bcc7093
Guillermo García Subirana <guillermogarsub@gmail.com>
2018-10-09 12:46:55 +0200

    Adde viewholder

---
+++ app/src/main/java/com/example/guillermogs2/countrylist/CountryViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.guillermogs2.countrylist;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class CountryViewHolder extends RecyclerView.ViewHolder{
+
+    private TextView country_view;
+
+    public CountryViewHolder(View itemView) {
+        super(itemView);
+        country_view = itemView.findViewById(R.id.country_view);
+    }
+
+    public void bind(String country){
+        country_view.setText(country);
+    }
+
+}
