20ae1433811df83645c1d1e535af62340dd22bea
Guillermo García Subirana <guillermogarsub@gmail.com>
2018-10-11 11:32:47 +0200

    Recolected the selected country from CountryListActivity

--- app/src/main/java/com/example/guillermogs2/countrylist/ChooseCountryActivity.java
+++ app/src/main/java/com/example/guillermogs2/countrylist/ChooseCountryActivity.java
@@ -1,25 +1,45 @@
 package com.example.guillermogs2.countrylist;
 
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ChooseCountryActivity extends AppCompatActivity {
+
+    public static final int CHOOSE_COUNTRY = 0;
+    private TextView country_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_country);
 
+        country_view = findViewById(R.id.country_view);
         Button btn_choose = findViewById(R.id.btn_choose);
         btn_choose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ChooseCountryActivity.this, CountryListActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, CHOOSE_COUNTRY);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            /*Recolect the result from CountryListActivity*/
+            case CHOOSE_COUNTRY:
+                if(resultCode == RESULT_OK){
+                    String country = data.getStringExtra("country");
+                    country_view.setText(country);
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
